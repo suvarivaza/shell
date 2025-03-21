f487a715a84febbe0ccc52bd24f113a5cdccba3d
suvarivaza <60654614+suvarivaza@users.noreply.github.com>
2025-03-20 17:04:48 -0700

    fix sever_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -5,16 +5,17 @@ block_access_by_password() {
     sudo sed -i 's/^PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config &&
     sudo sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config &&
     sudo sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config &&
-    restart_ssh && echo "OK!"
+    grep PubkeyAuthentication /etc/ssh/sshd_config && grep PasswordAuthentication /etc/ssh/sshd_config &&
+    restart_ssh
 }
 
 restart_ssh() {
   if grep -qi "ubuntu" /etc/os-release; then
     #      echo "Detected Ubuntu, restarting SSH..."
-    sudo systemctl restart ssh
+    sudo systemctl restart ssh && echo "OK! service ssh restarted"
   elif grep -qi "centos" /etc/os-release; then
     #      echo "Detected CentOS, restarting SSH..."
-    sudo systemctl restart sshd
+    sudo systemctl restart sshd && echo "OK! service sshd restarted"
   else
     echo "Unknown OS, SSH restart command not executed."
   fi
