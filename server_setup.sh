802d6cfb3898b429820cc20c27cbbbf856d22d4e
Mikhail Lushnikov <suvarivaza@Mikhails-MacBook-Pro.local>
2025-04-17 14:25:15 -0700

    some fixes

--- server_setup.sh
+++ server_setup.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+RESET='\033[0m'  # Reset to default color
+
+
+check_ssh_settings(){
+  echo -e "${CYAN}==================== Проверяем текущие SSH настройки ====================${RESET}"
+  grep PubkeyAuthentication /etc/ssh/sshd_config && grep PasswordAuthentication /etc/ssh/sshd_config
+}
+
 block_access_by_password() {
+    echo -e "${CYAN}==================== Блокируем доступ к SSH по паролю ====================${RESET}"
+
     sudo sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config &&
     sudo sed -i 's/^#PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config &&
     sudo sed -i 's/^PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config &&
     sudo sed -i 's/^#PasswordAuthentication no/PasswordAuthentication no/' /etc/ssh/sshd_config &&
     sudo sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config &&
     sudo sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config &&
-    grep PubkeyAuthentication /etc/ssh/sshd_config && grep PasswordAuthentication /etc/ssh/sshd_config &&
-    restart_ssh
+    restart_ssh && check_ssh_settings
 }
 
 restart_ssh() {
@@ -27,13 +42,15 @@ restart_ssh() {
 while true; do
   echo
   echo "Выберите действие:"
-  echo "1. Заблокировать доступ по паролю"
+  echo "1. Проверить SSH настройки"
+  echo "2. Заблокировать доступ к SSH по паролю"
   echo "0. Выход"
   echo
   read -p "Введите номер действия: " CHOICE
 
   case $CHOICE in
-  1) block_access_by_password ;;
+  1) check_ssh_settings ;;
+  2) block_access_by_password ;;
   0)
     echo "Выход."
     exit 0
